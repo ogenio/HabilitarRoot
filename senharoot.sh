74003d6ae5da40254f6fe3c7943c6872e884ba4a
fabricio94b <60627225+fabricio94b@users.noreply.github.com>
2021-06-28 13:13:40 -0300

    Update senharoot.sh

--- senharoot.sh
+++ senharoot.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 clear
-# By crazy_vpn
-# Pequeno script para permissao de autenticacao root!!
-
 [[ $(grep -c "prohibit-password" /etc/ssh/sshd_config) != '0' ]] && {
 	sed -i "s/prohibit-password/yes/g" /etc/ssh/sshd_config
 } > /dev/null
